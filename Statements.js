d5a5c4abbd3e7ecf08dd53d4ffc217d0c85e8ed7
Mansi Mahant <mahantmansi22@gmail.com>
2024-07-17 23:07:56 +0530

    Add: Statements

--- Statements.js
+++ Statements.js
@@ -3,4 +3,4 @@ a = 14; // Assign the value 14 to a
 b = 20; // Assign the value 20 to b
 c = a + b; // Assign the sum of a and b to c
 
-console.log("Output is: ", c); // Output: 34
+console.log("Output is: ", c); // Output is: 34
