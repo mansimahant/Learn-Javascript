7265411928cd48eec634404b4a92993ace7c8f0a
Mansi Mahant <mahantmansi22@gmail.com>
2024-07-17 23:17:00 +0530

    Add: Comments

---
+++ Comments.js
@@ -0,0 +1,10 @@
+// Single Line Comment
+// Javascript
+
+// ---------------------------
+
+/* Multi Line Comment */
+/*
+JavaScript code can be made more understandable and explained with the use of comments.
+When testing alternative code, JavaScript comments can also be used to stop the code from running.
+*/
