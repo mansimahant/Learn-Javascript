82ee76e0c8d5bcab278c67c98bed21e63f3b47bb
Mansi Mahant <mahantmansi22@gmail.com>
2024-07-17 23:06:44 +0530

    Add: Statements

---
+++ Statements.js
@@ -0,0 +1,6 @@
+let a, b, c; // Declare 3 variables
+a = 14; // Assign the value 14 to a
+b = 20; // Assign the value 20 to b
+c = a + b; // Assign the sum of a and b to c
+
+console.log("Output is: ", c); // Output: 34
