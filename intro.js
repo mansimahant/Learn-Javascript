a4d21df5f4f1e8181a287ba42080f6ae50ec516f
Mansi Mahant <mahantmansi22@gmail.com>
2024-07-17 22:32:39 +0530

    first commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+// Originally, JavaScript was developed to "make web pages alive."
+
+// Scripts are the term for programs written in this language.They can be programmed directly into the HTML of a web page to execute automatically as the page loaded.
+
+// Scripts are given as plain text to be run.For them to function, no extra setup or compilation is required.
